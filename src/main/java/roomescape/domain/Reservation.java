a5f27b78e4a25996ced5aa5eac9c57a2ebdd6fe6
junseoplee <junseoplee@soongsil.ac.kr>
2024-08-04 21:03:01 +0900

    feat: 각 상황에 따라 더 세부적인 메세지를 지정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,16 +4,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Objects;
 
 public class Reservation {
 
   private final Long id;
-  @NotBlank
+
+  @NotBlank(message = "ErrorMessage.EMPTY_NAME.getMessage()")
   private final String name;
-  @NotNull
+
+  @NotNull(message = "ErrorMessage.EMPTY_DATE.getMessage()")
   private final LocalDate date;
-  @NotNull
+
+  @NotNull(message = "ErrorMessage.EMPTY_TIME.getMessage()")
   private final LocalTime time;
 
   public Reservation(Long id, String name, LocalDate date, LocalTime time) {
@@ -27,7 +29,7 @@ public class Reservation {
     return new Reservation(id, reservation.name, reservation.date, reservation.time);
   }
 
-  public long getId() {
+  public Long getId() {
     return id;
   }
 
@@ -41,21 +43,5 @@ public class Reservation {
 
   public LocalTime getTime() {
     return time;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof Reservation reservation)) {
-      return false;
-    }
-    return Objects.equals(id, reservation.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id);
   }
 }
